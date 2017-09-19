2156e9468ad96df082de58a43ebd5452e8e4e1a9
Alexander Bock <alexander.bock@me.com>
2017-09-19 00:53:47 -0400

    Add Walking Manhattan to project

---
+++ assets/project_walkingmanhattan.js
@@ -0,0 +1,44 @@
+GMap2.prototype.fit = function(bounds) {
+  this.setCenter(bounds.getCenter(), this.getBoundsZoomLevel(bounds));
+};
+
+$(document).ready(function() {
+  var map = new GMap2(document.getElementById("map_canvas"));
+
+  bounds = new GLatLngBounds(
+    new GLatLng(40.693741, -73.99),
+    new GLatLng(40.788675, -73.985)
+  );
+  
+  map.fit(bounds);
+  map.setUIToDefault();
+  map.setMapType(G_PHYSICAL_MAP);
+
+  $.get(
+    'assets/project_walkingmanhattan.data',
+    null,
+    function(data) {
+      var lines = data.split('\t')
+      for (var line = 0; line < lines.length; ++line) {
+        var values = lines[line].split(' ')
+
+        var q;
+        for (var value = 0; value < values.length; value=value+2) {
+          var lat = values[value];
+          var lng = values[value + 1];
+          var p = new GLatLng(parseFloat(lat, 10), parseFloat(lng, 10));
+
+          if (value > 0) {
+            map.addOverlay(
+              //                    Color      Line Width   Opacity
+              new GPolyline([q, p], "#b5282f", 3,           0.5)
+            );
+          }
+
+          q = p;
+        }
+      }
+    },
+    'text'
+  );
+});
