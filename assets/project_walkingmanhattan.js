09e74564a463e176636c21d439c5828d8d045803
Alexander Bock <alexander.bock@me.com>
2017-09-21 09:47:26 -0400

    Update to new Google Maps API

--- assets/project_walkingmanhattan.js
+++ assets/project_walkingmanhattan.js
@@ -1,18 +1,11 @@
-GMap2.prototype.fit = function(bounds) {
-  this.setCenter(bounds.getCenter(), this.getBoundsZoomLevel(bounds));
-};
-
 $(document).ready(function() {
-  var map = new GMap2(document.getElementById("map_canvas"));
-
-  bounds = new GLatLngBounds(
-    new GLatLng(40.693741, -73.99),
-    new GLatLng(40.788675, -73.985)
+  var map = new google.maps.Map(
+    document.getElementById("map_canvas"),
+    {
+      zoom: 12,
+      center: { lat: 40.72, lng: -73.99 }
+    }
   );
-  
-  map.fit(bounds);
-  map.setUIToDefault();
-  map.setMapType(G_PHYSICAL_MAP);
 
   $.get(
     'assets/project_walkingmanhattan.data',
@@ -22,22 +15,26 @@ $(document).ready(function() {
       for (var line = 0; line < lines.length; ++line) {
         var values = lines[line].split(' ')
 
-        var q;
+        points = []
         // We have to stop one iteration earlier due to an extra " " at the end of the the line
         for (var value = 0; value < values.length; value=value+2) {
-          var lat = values[value];
-          var lng = values[value + 1];
-          var p = new GLatLng(parseFloat(lat, 10), parseFloat(lng, 10));
+          var lat = parseFloat(values[value], 10);
+          var lng = parseFloat(values[value + 1], 10);
 
-          if (value > 0) {
-            map.addOverlay(
-              //                    Color      Line Width   Opacity
-              new GPolyline([q, p], "#b5282f", 3,           0.5)
-            );
-          }
-
-          q = p;
+          points.push({
+            lat: lat,
+            lng: lng
+          });
         }
+
+        new google.maps.Polyline({
+          path: points,
+          geodesic: true,
+          strokeColor: "#b5282f",
+          strokeOpacity: 0.95,
+          strokeWeight: 3,
+          map: map
+        });
       }
     },
     'text'
