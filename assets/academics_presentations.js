fc7a9081d7e657f377ca8135f1fa104b325f4707
Alexander Bock <mail@alexanderbock.eu>
2019-12-12 14:51:19 +0100

    Update teaching and presentations

--- assets/academics_presentations.js
+++ assets/academics_presentations.js
@@ -1,208 +1,232 @@
 var json = {
-    "presentations": [
-        {
-            "occasion"  : "Data Visualization Summit",
-            "title"     : "OpenSpace: Moving from What to How in Astrovisualization",
-            "location"  : "Boston, USA",
-            "year"      : "2018"
-        },
-        {
-            "occasion"  : "SIGGRAPH",
-            "title"     : "Globe Browsing: Contextualized Spatio-Temporal Planetary Surface Visualization",
-            "location"  : "Vancouver, Canada",
-            "year"      : "2018"
-        },
-        {
-            "occasion"  : "Thesis Defense",
-            "title"     : "Tailoring Visualization Applications for Tasks and Users",
-            "location"  : "Norrköping, Sweden",
-            "year"      : "2018"
-        },
-        {
-            "occasion"  : "Evolutionary Robotics",
-            "title"     : "OpenSpace: An open-source astrovisualization framework bridging the gap between scientists and the public",
-            "location"  : "online",
-            "year"      : "2017",
-            "video"     : "https://www.youtube.com/watch?v=iWP0j8UsWLM"
-        },
-        {
-            "occasion"  : "Flatiron Scientific Visualization Conference",
-            "title"     : "OpenSpace: Hands-On",
-            "location"  : "New York, USA",
-            "year"      : "2017",
-            "type"      : "Invited"
-        },
-        {
-            "occasion"  : "IEEE Vis",
-            "title"     : "TopoAngler: Interactive Topology-based Segmentation of Fishes",
-            "location"  : "Phoenix, USA",
-            "year"      : "2017",
-            "type"      : "Paper"
-        },
-        {
-            "occasion"  : "Space Apps NYC",
-            "title"     : "OpenSpace",
-            "location"  : "New York, USA",
-            "year"      : "2017",
-            "type"      : "Invited",
-            "video"     : "https://www.youtube.com/watch?v=p8UtGyKFdRo"
-        },
-        {
-            "occasion"  : "Center for Data Science Lunch Series",
-            "title"     : "OpenSpace: Moving from What to How in Science Communication",
-            "location"  : "New York, USA",
-            "year"      : "2017",
-            "type"      : "Seminar"
-        },
-        {
-            "occasion"  : "MIT Media Lab",
-            "title"     : "OpenSpace: Moving from What to How in Science Communication",
-            "location"  : "Boston, USA",
-            "year"      : "2017",
-            "type"      : "Invited"
-        },
-        {
-            "occasion"  : "Crossdrive",
-            "title"     : "OpenSpace: From What to How in Science Communication",
-            "location"  : "Turin, Italy",
-            "year"      : "2016",
-            "type"      : "Invited"
-        },
-        {
-            "occasion"  : "EuroVis",
-            "title"     : "Hybrid Data Visualization Based on Depth Complexity Histogram Analysis",
-            "location"  : "Groningen, The Netherlands",
-            "year"      : "2016",
-            "type"      : "Paper",
-            "video"     : "https://vimeo.com/187725634"
-        },
-        {
-            "occasion"  : "EuroVis",
-            "title"     : "OpenSpace: Visualizing the Universe",
-            "location"  : "Groningen, The Netherlands",
-            "year"      : "2016",
-            "type"      : "Keynote*",
-            "video"     : "https://vimeo.com/169967499"
-        },
-        {
-            "occasion"  : "SIGRAD",
-            "title"     : "Hybrid Data Visualization Based on Depth Complexity Histogram Analysis",
-            "location"  : "Visby, Sweden",
-            "year"      : "2016",
-            "type"      : "Paper"
-        },
-        {
-            "occasion"  : "International CCMC Workshop",
-            "title"     : "OpenSpace: A Collaborative Science Dissemination Tool",
-            "location"  : "Annapolis, USA",
-            "year"      : "2016",
-            "type"      : "Invited"
-        },
-        {
-            "occasion"  : "American Geophysical Union, Fall Meeting",
-            "title"     : "OpenSpace: An Open-Source Framework for Data Visualization and Contextualization",
-            "location"  : "San Francisco, USA",
-            "year"      : "2015",
-            "type"      : "Abstract"
-        },
-        {
-            "occasion"  : "Vis",
-            "title"     : "Visual Verification of Space Weather Ensemble Simulations",
-            "location"  : "Chicago, USA",
-            "year"      : "2015",
-            "type"      : "Paper"
-        },
-        {
-            "occasion"  : "Laboratory of Organic Electronic Seminars",
-            "title"     : "Visualization. Why? How? What?",
-            "location"  : "Norrköping, Sweden",
-            "year"      : "2015",
-            "type"      : "Invited"
-        },
-        {
-            "occasion"  : "MSB Emergency Preparedness Seminar",
-            "title"     : "An Interactive Visualization System for Urban Search & Rescue Mission Planning",
-            "location"  : "Katrineholm, Sweden",
-            "year"      : "2015",
-            "type"      : "Invited"
-        },
-        {
-            "occasion"  : "Campusveckan",
-            "title"     : "Visualization and its Applications",
-            "location"  : "Norrköping, Sweden",
-            "year"      : "2015",
-            "type"      : "Introductory"
-        },
-        {
-            "occasion"  : "1st KHA/AUT Computer Science/Engineering Seminar",
-            "title"     : "Introduction to Scientific Visualization and its Applications",
-            "location"  : "Tehran, Iran",
-            "year"      : "2015",
-            "type"      : "Invited"
-        },
-        {
-            "occasion"  : "Symposium on Safety, Security, and Rescue Robotics",
-            "title"     : "An Interactive Visualization System for Urban Search & Rescue Mission Planning",
-            "location"  : "Toyako-cho, Japan",
-            "year"      : "2014",
-            "type"      : "Paper"
-        },
-        {
-            "occasion"  : "Vision, Modelling, and Visualization",
-            "title"     : "Supporting Urban Search & Rescue Mission Planning Through Visualization-Based Analysis",
-            "location"  : "Darmstadt, Germany",
-            "year"      : "2014",
-            "type"      : "Paper"
-        },
-        {
-            "occasion"  : "Solar Information Processing Workshop",
-            "title"     : "Towards Visual Verification of Coronal Mass Ejections in ENLIL Ensemble Simulations through Optical Flow Analysis",
-            "location"  : "La Roche-en-Ardenne, Belgium",
-            "year"      : "2014",
-            "type"      : "Invited"
-        },
-        {
-            "occasion"  : "International CCMC Workshop",
-            "title"     : "CCMC-LiU-AMNH Partnership: Advanced Visualization",
-            "location"  : "Annapolis, MD, USA",
-            "year"      : "2014",
-            "type"      : "Invited"
-        },
-        {
-            "occasion"  : "SIGRAD",
-            "title"     : "Poor Man's Rendering of Segmented Data",
-            "location"  : "Norrköping, Sweden",
-            "year"      : "2013",
-            "type"      : "Paper"
-        },
-        {
-            "occasion"  : "PacificVis",
-            "title"     : "Guiding Deep Brain Stimulation Interventions by Fusing Multimodal Uncertainty Regions",
-            "location"  : "Sydney, Australia",
-            "year"      : "2013",
-            "type"      : "Paper"
-        },
-        {
-            "occasion"  : "Vis",
-            "title"     : "Coherency-Based Curve Compression for High-Order Finite Element Model Visualization",
-            "location"  : "Seattle, USA",
-            "year"      : "2012",
-            "type"      : "Paper"
-        },
-        {
-            "occasion"  : "Swedish e-Science Research Center",
-            "title"     : "Voreen",
-            "location"  : "Norrköping, Sweden",
-            "year"      : "2012",
-            "type"      : "Workshop"
-        },
-    ]
+  "presentations": [
+    {
+      "occasion"  : "IEEE Vis",
+      "title"     : "OpenSpace: A system for astrographics",
+      "location"  : "Vancouver, Canada",
+      "year"      : "2019"
+    },
+    {
+      "occasion"  : "Dagstuhl Seminar Series",
+      "title"     : "Introductory presentation",
+      "location"  : "Schloss Dagstuhl, Germany",
+      "year"      : "2019"
+    },
+    {
+      "occasion"  : "IEEE Vis",
+      "title"     : "OpenSpace: Changing the Narrative of Public Dissemination in Astronomical Visualization from What to How",
+      "location"  : "Berlin, Germany",
+      "year"      : "2018"
+    },
+    {
+      "occasion"  : "Moore-Sloan Data Science Environment Meeting",
+      "title"     : "OpenSpace: Moving from What to How in Public Dissemination",
+      "location"  : "Park City, USA",
+      "year"      : "2018"
+    },
+    {
+      "occasion"  : "Data Visualization Summit",
+      "title"     : "OpenSpace: Moving from What to How in Astrovisualization",
+      "location"  : "Boston, USA",
+      "year"      : "2018"
+    },
+    {
+      "occasion"  : "SIGGRAPH",
+      "title"     : "Globe Browsing: Contextualized Spatio-Temporal Planetary Surface Visualization",
+      "location"  : "Vancouver, Canada",
+      "year"      : "2018"
+    },
+    {
+      "occasion"  : "Thesis Defense",
+      "title"     : "Tailoring Visualization Applications for Tasks and Users",
+      "location"  : "Norrköping, Sweden",
+      "year"      : "2018"
+    },
+    {
+      "occasion"  : "Evolutionary Robotics",
+      "title"     : "OpenSpace: An open-source astrovisualization framework bridging the gap between scientists and the public",
+      "location"  : "online",
+      "year"      : "2017",
+      "video"     : "https://www.youtube.com/watch?v=iWP0j8UsWLM"
+    },
+    {
+      "occasion"  : "Flatiron Scientific Visualization Conference",
+      "title"     : "OpenSpace: Hands-On",
+      "location"  : "New York, USA",
+      "year"      : "2017",
+      "type"      : "Invited"
+    },
+    {
+      "occasion"  : "IEEE Vis",
+      "title"     : "TopoAngler: Interactive Topology-based Segmentation of Fishes",
+      "location"  : "Phoenix, USA",
+      "year"      : "2017",
+      "type"      : "Paper"
+    },
+    {
+      "occasion"  : "Space Apps NYC",
+      "title"     : "OpenSpace",
+      "location"  : "New York, USA",
+      "year"      : "2017",
+      "type"      : "Invited",
+      "video"     : "https://www.youtube.com/watch?v=p8UtGyKFdRo"
+    },
+    {
+      "occasion"  : "Center for Data Science Lunch Series",
+      "title"     : "OpenSpace: Moving from What to How in Science Communication",
+      "location"  : "New York, USA",
+      "year"      : "2017",
+      "type"      : "Seminar"
+    },
+    {
+      "occasion"  : "MIT Media Lab",
+      "title"     : "OpenSpace: Moving from What to How in Science Communication",
+      "location"  : "Boston, USA",
+      "year"      : "2017",
+      "type"      : "Invited"
+    },
+    {
+      "occasion"  : "Crossdrive",
+      "title"     : "OpenSpace: From What to How in Science Communication",
+      "location"  : "Turin, Italy",
+      "year"      : "2016",
+      "type"      : "Invited"
+    },
+    {
+      "occasion"  : "EuroVis",
+      "title"     : "Hybrid Data Visualization Based on Depth Complexity Histogram Analysis",
+      "location"  : "Groningen, The Netherlands",
+      "year"      : "2016",
+      "type"      : "Paper",
+      "video"     : "https://vimeo.com/187725634"
+    },
+    {
+      "occasion"  : "EuroVis",
+      "title"     : "OpenSpace: Visualizing the Universe",
+      "location"  : "Groningen, The Netherlands",
+      "year"      : "2016",
+      "type"      : "Keynote*",
+      "video"     : "https://vimeo.com/169967499"
+    },
+    {
+      "occasion"  : "SIGRAD",
+      "title"     : "Hybrid Data Visualization Based on Depth Complexity Histogram Analysis",
+      "location"  : "Visby, Sweden",
+      "year"      : "2016",
+      "type"      : "Paper"
+    },
+    {
+      "occasion"  : "International CCMC Workshop",
+      "title"     : "OpenSpace: A Collaborative Science Dissemination Tool",
+      "location"  : "Annapolis, USA",
+      "year"      : "2016",
+      "type"      : "Invited"
+    },
+    {
+      "occasion"  : "American Geophysical Union, Fall Meeting",
+      "title"     : "OpenSpace: An Open-Source Framework for Data Visualization and Contextualization",
+      "location"  : "San Francisco, USA",
+      "year"      : "2015",
+      "type"      : "Abstract"
+    },
+    {
+      "occasion"  : "Vis",
+      "title"     : "Visual Verification of Space Weather Ensemble Simulations",
+      "location"  : "Chicago, USA",
+      "year"      : "2015",
+      "type"      : "Paper"
+    },
+    {
+      "occasion"  : "Laboratory of Organic Electronic Seminars",
+      "title"     : "Visualization. Why? How? What?",
+      "location"  : "Norrköping, Sweden",
+      "year"      : "2015",
+      "type"      : "Invited"
+    },
+    {
+      "occasion"  : "MSB Emergency Preparedness Seminar",
+      "title"     : "An Interactive Visualization System for Urban Search & Rescue Mission Planning",
+      "location"  : "Katrineholm, Sweden",
+      "year"      : "2015",
+      "type"      : "Invited"
+    },
+    {
+      "occasion"  : "Campusveckan",
+      "title"     : "Visualization and its Applications",
+      "location"  : "Norrköping, Sweden",
+      "year"      : "2015",
+      "type"      : "Introductory"
+    },
+    {
+      "occasion"  : "1st KHA/AUT Computer Science/Engineering Seminar",
+      "title"     : "Introduction to Scientific Visualization and its Applications",
+      "location"  : "Tehran, Iran",
+      "year"      : "2015",
+      "type"      : "Invited"
+    },
+    {
+      "occasion"  : "Symposium on Safety, Security, and Rescue Robotics",
+      "title"     : "An Interactive Visualization System for Urban Search & Rescue Mission Planning",
+      "location"  : "Toyako-cho, Japan",
+      "year"      : "2014",
+      "type"      : "Paper"
+    },
+    {
+      "occasion"  : "Vision, Modelling, and Visualization",
+      "title"     : "Supporting Urban Search & Rescue Mission Planning Through Visualization-Based Analysis",
+      "location"  : "Darmstadt, Germany",
+      "year"      : "2014",
+      "type"      : "Paper"
+    },
+    {
+      "occasion"  : "Solar Information Processing Workshop",
+      "title"     : "Towards Visual Verification of Coronal Mass Ejections in ENLIL Ensemble Simulations through Optical Flow Analysis",
+      "location"  : "La Roche-en-Ardenne, Belgium",
+      "year"      : "2014",
+      "type"      : "Invited"
+    },
+    {
+      "occasion"  : "International CCMC Workshop",
+      "title"     : "CCMC-LiU-AMNH Partnership: Advanced Visualization",
+      "location"  : "Annapolis, MD, USA",
+      "year"      : "2014",
+      "type"      : "Invited"
+    },
+    {
+      "occasion"  : "SIGRAD",
+      "title"     : "Poor Man's Rendering of Segmented Data",
+      "location"  : "Norrköping, Sweden",
+      "year"      : "2013",
+      "type"      : "Paper"
+    },
+    {
+      "occasion"  : "PacificVis",
+      "title"     : "Guiding Deep Brain Stimulation Interventions by Fusing Multimodal Uncertainty Regions",
+      "location"  : "Sydney, Australia",
+      "year"      : "2013",
+      "type"      : "Paper"
+    },
+    {
+      "occasion"  : "Vis",
+      "title"     : "Coherency-Based Curve Compression for High-Order Finite Element Model Visualization",
+      "location"  : "Seattle, USA",
+      "year"      : "2012",
+      "type"      : "Paper"
+    },
+    {
+      "occasion"  : "Swedish e-Science Research Center",
+      "title"     : "Voreen",
+      "location"  : "Norrköping, Sweden",
+      "year"      : "2012",
+      "type"      : "Workshop"
+    },
+  ]
 }
 
 // Apply the handlebars
 $(function () {
-    var theTemplateScript = $("#presentation-template").html();
-    var theTemplate = Handlebars.compile(theTemplateScript);
-    var theCompiledHtml = theTemplate(json);
-    $('.content-placeholder').html(theCompiledHtml);
+  var theTemplateScript = $("#presentation-template").html();
+  var theTemplate = Handlebars.compile(theTemplateScript);
+  var theCompiledHtml = theTemplate(json);
+  $('.content-placeholder').html(theCompiledHtml);
 })
